e89af9bfe7e4b7a7ee9953450743b0f5444a2e0f
Aruna Priyadarshana Bandara <arunapbandara45@gmail.com>
2024-10-26 04:02:59 +0530

    initiate frontend

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Card } from "@/components/ui/card";
+
+
+export default function Home() {
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center min-h-screen">
+      <Card>
+      <h1 className="text-7xl font-bold">EAD Frontend</h1>
+        
+      </Card>
+    </div>
+  );
+}
---
+++ services/axiosInstance.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const BaseUrl = process.env.BASE_URL || "https://localhost:8080/api/v1";
+
+const axiosInstance = axios.create({
+  baseURL: BaseUrl,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
