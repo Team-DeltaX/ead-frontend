7e54308380429ca5d0efdc6ced95c05e2198834e
Chathura Ishara <112225216+ChathuraIshara@users.noreply.github.com>
2024-11-15 00:01:51 +0530

    payment ui pages initialized

---
+++ components/payment/AddressBox.tsx
@@ -0,0 +1,39 @@
+import { CheckCircle } from "lucide-react";
+import React from "react";
+import { MdCheckBox } from "react-icons/md";
+
+function AddressBox() {
+  return (
+    <div className="flex justify-between p-4 bg-gray-200 w-[1000] rounded-md h-28">
+      {/*selector and address*/}
+      <div className="flex">
+        {/*selector*/}
+        <div className="mr-4">
+          {/*radio button*/}
+          <input
+            type="radio"
+            name="address"
+            style={{
+              width: "1.25rem",
+              height: "1.25rem",
+              accentColor: "black", // Makes the selected color black
+            }}
+            
+          />
+        </div>
+        {/*address*/}
+        <div>
+            <span className="font-extralight">2118 Tharindage</span><br></br>
+            <span className="text-sm text-gray-500 font-light">2118 Tharindage Cir.Syracue,Connecticut 35624</span><br></br>
+            <span className="text-sm text-gray-500 font-light">(209) 555-0104</span>
+        </div>
+      </div>
+      {/*button*/}
+      <div>
+        <CheckCircle></CheckCircle>
+      </div>
+    </div>
+  );
+}
+
+export default AddressBox;
---
+++ components/payment/NewAddressDialog.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { Dialog,DialogContent,DialogDescription,DialogFooter,DialogHeader,DialogTitle,DialogTrigger } from '../ui/dialog';
+import { Input } from '../ui/input';
+import { Label } from "../ui/label";
+import {Button} from '../ui/button';
+import { CirclePlus } from 'lucide-react';
+
+function NewAddressDialog() {
+  
+    return (
+        <Dialog>
+          <DialogTrigger asChild>
+          <button><CirclePlus></CirclePlus></button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Add new Address</DialogTitle>
+              <DialogDescription>
+                Make sure to Add valid address. Click save when you're done.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="name" className="text-right">
+                  Name
+                </Label>
+                <Input
+                  id="name"
+                  defaultValue="Pedro Duarte"
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="username" className="text-right">
+                  Address
+                </Label>
+                <Input
+                  id="username"
+                  defaultValue="GalleRoad,Katubedda"
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="username" className="text-right">
+                  Phone
+                </Label>
+                <Input
+                  id="username"
+                  defaultValue="0112256475"
+                  className="col-span-3"
+                />
+              </div>
+            </div>
+            <DialogFooter>
+              <Button type="submit">Add New</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      )
+  
+}
+
+export default NewAddressDialog
