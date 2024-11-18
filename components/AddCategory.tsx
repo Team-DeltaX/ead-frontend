efa9f04a28ad16fc510cae5a6956f1bf7cec972d
Ashen Wijesinghe <142490299+Ashen06C@users.noreply.github.com>
2024-11-18 12:30:59 +0530

    category and order page

---
+++ components/AddCategory.tsx
@@ -0,0 +1,49 @@
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export function AddCategory() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+      <Button
+          variant="outline"
+          className="mt-1 bg-gray-500 hover:bg-gray-600 text-white py-2 px-4 rounded focus:border-black shadow-md"
+        >
+          Add Category
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add Category</DialogTitle>
+          <DialogDescription>
+            Add new Categories here. Click save when you're done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Name
+            </Label>
+            <Input
+              id="name"
+              className="col-span-3"
+            />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  )
+}
