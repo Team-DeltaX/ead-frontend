093b498cb15a73d6c5e2dad2179438e87735bb60
charukabandara22 <144276388+charukabandara22@users.noreply.github.com>
2024-11-21 22:48:53 +0530

    create product page

---
+++ app/product/ProductCard.jsx
@@ -0,0 +1,31 @@
+import { useRouter } from 'next/navigation'
+import React from "react";
+
+export const ProductCard = ({ product }) => {
+  const router = useRouter()
+
+  return (
+    <div className="max-w-sm p-4 border rounded-lg shadow-md bg-[#F6F6F6]">
+      <img
+        src={product.image}
+        alt={product.name}
+        className="w-full object-cover rounded-lg p-4"
+      />
+      <div className="mt-4 text-center">
+        <h3 className="text-base font-medium text-gray-900">{product.name}</h3>
+        <p className="text-xs text-gray-500">{product.brand}</p>
+        <p className="mt-1 text-lg font-bold text-gray-800">${product.price}</p>
+        <button
+          className="mt-2 px-4 py-1 bg-black text-white rounded-lg hover:bg-gray-800"
+          onClick={()=>{
+            
+            router.push(`/product/${product.id}`);
+
+          }}
+        >
+          Buy Now
+        </button>     
+      </div>
+    </div>
+  );
+};
