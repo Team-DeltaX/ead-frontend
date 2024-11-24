6bdec51bcae9a94fe935ab8196a42e02549e70d6
Aruna Priyadarshana Bandara <arunapbandara45@gmail.com>
2024-11-24 20:30:05 +0530

    fix error

--- services/user.service.ts
+++ services/user.service.ts
@@ -1,17 +1,22 @@
 import axiosInstance from "./axiosInstance";
+import { Order } from "./order.service";
 
 export interface User {
   id?: number;
-  firstname: string;
+  firstName: string;
+  lastName: string;
   email: string;
   password: string;
+  phone: string;
+  address: string;
+  orders: Order[];
 }
 
 export const userService = {
 
   getUserById : async () => {
     const response = await axiosInstance.get(`/users`);
-    return response.data;
+    return response.data
   }
 
 }
