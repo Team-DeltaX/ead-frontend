08b965b08cd5a0a8153023dd41cc70f1e39dd54f
Chathura Ishara <112225216+ChathuraIshara@users.noreply.github.com>
2024-12-05 09:16:20 +0530

    Update userProfile.cy.ts
    added user profile tests

--- cypress/e2e/userProfile/userProfile.cy.ts
+++ cypress/e2e/userProfile/userProfile.cy.ts
@@ -2,8 +2,6 @@
 
 describe('Profile Details Functionality', () => {
   beforeEach(() => {
-    // Intercept the user details API request
-    cy.intercept('GET', '/api/user', { fixture: 'users.json' }).as('getUser');
     
     // Visit the homepage and log in
     cy.visit('http://localhost:3000/');
@@ -17,8 +15,6 @@ describe('Profile Details Functionality', () => {
     // Navigate to the profile page
     cy.get('[data-cy="profileicon"]').click();
 
-    // Wait for the intercepted API call
-    cy.wait('@getUser');
   });
 
   it('Profile page should be displayed correctly', () => {
@@ -30,10 +26,10 @@ describe('Profile Details Functionality', () => {
 
   it('Displays user details correctly', () => {
     // Verify the user details are displayed correctly
-    cy.get('label[for="name"]').contains('First Name').should('contain.text', 'John'); // First Name from fixture
-    cy.get('label[for="name"]').contains('Last Name').should('contain.text', 'Doe'); // Last Name from fixture
-    cy.get('label[for="name"]').contains('E-mail').should('contain.text', 'john.doe@example.com'); // E-mail from fixture
-    cy.get('label[for="name"]').contains('Phone').should('contain.text', '1234567890'); // Phone from fixture
-    cy.get('label[for="name"]').contains('Address').should('contain.text', '123 Main St'); // Address from fixture
+    cy.get('label[for="name"]').contains('First Name').should('contain.text', 'chathura'); 
+    cy.get('label[for="name"]').contains('Last Name').should('contain.text', 'ishara'); 
+    cy.get('label[for="name"]').contains('E-mail').should('contain.text', 'chathuraishara63@gmail.com'); 
+    cy.get('label[for="name"]').contains('Phone').should('contain.text', '0774733245'); 
+    cy.get('label[for="name"]').contains('Address').should('contain.text', ''); 
   });
 });
