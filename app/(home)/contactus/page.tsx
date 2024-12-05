86de6116427c75127cdd5df5d23fa5c520603fee
Samitha Wijenayake <samithamahesh@gmail.com>
2024-12-05 13:31:00 +0530

    Fixed errors in Contact Us  page

--- app/(home)/contactus/page.tsx
+++ app/(home)/contactus/page.tsx
@@ -7,7 +7,7 @@ export default function ContactUs() {
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-50 via-white to-blue-100 font-SFPro">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10 max-w-5xl w-full p-8 shadow-lg rounded-xl bg-white">
         <div className="col-span-full text-center mb-6">
-          <h1 className="text-6xl text-black-600" style={{ fontWeight: 900 }}>
+          <h1 className="text-6xl text-black-600" style={{ fontWeight: 950 }}>
             Contact Us
           </h1>
           <p className="text-gray-600 mt-2">We&apos;d love to hear from you!</p>
@@ -16,7 +16,7 @@ export default function ContactUs() {
         <div className="flex justify-center items-center">
           <div className="flex flex-col justify-center">
             <div className="mb-8">
-              <h2 className="text-xl font-semibold text-blue-500 mb-3">
+              <h2 className="text-xl font-semibold text-blue-500 ">
                 Visit us
               </h2>
               <p className="text-gray-600 leading-relaxed">
@@ -51,29 +51,10 @@ export default function ContactUs() {
           </div>
         </div>
 
-        <div className="flex flex-col justify-center">
-          <h1 className="text-5xl font-black text-gray-800 mb-6 leading-tight">
-            Contact Us
-          </h1>
+        <div className="flex flex-col ">
+        
           <div className="mb-8">
-            <h2 className="text-xl font-semibold text-blue-500 mb-3">
-              Visit us
-            </h2>
-            <p className="text-gray-600 leading-relaxed">
-              2972 Westheimer Rd.
-              <br />
-              Santa Ana, Illinois
-              <br />
-              85486
-            </p>
-            <p className="mt-3 text-gray-600">
-              contact@company.com
-              <br />
-              (406) 555-0120
-            </p>
-          </div>
-          <div className="mb-8">
-            <h2 className="text-xl font-semibold text-blue-500 mb-3">
+            <h2 className="text-xl font-semibold text-blue-500 ">
               Send a Message
             </h2>
             <p className="text-gray-600">
@@ -103,12 +84,7 @@ export default function ContactUs() {
               Send
             </button>
           </form>
-          <button
-            className="bg-gray-900 text-white px-8 py-3 rounded-lg shadow-md hover:bg-gray-700 transition-all duration-300"
-            onClick={() => alert("Navigating to Careers!")}
-          >
-            Next
-          </button>
+         
         </div>
       </div>
     </div>
